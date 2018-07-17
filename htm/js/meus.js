cdaa2a4f2970cb3df9dc87403a8405cc9f207edd
graonatural <39735048+graonatural@users.noreply.github.com>
2018-07-17 10:51:17 -0300

    Sent by DeployFC at 17/07/2018 10:51:17

---
+++ htm/js/meus.js
@@ -0,0 +1,5 @@
+function LinkDisp(Estoque,IDProduto){
+ if(Estoque==0){
+ document.write("<br>Produto disponível apenas para compra direta na loja ou tele-entrega. Caso tenha interesse entre em contato conosco pelo email contato@graonatural.com.br ou pelo telefone (51) 3061-6240");
+ }
+}
