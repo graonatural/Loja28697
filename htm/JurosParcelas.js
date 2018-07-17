14f063d2b61ab3b219856c485a02bed7ddb55aab
graonatural <39735048+graonatural@users.noreply.github.com>
2018-07-17 10:53:39 -0300

    Sent by DeployFC at 17/07/2018 10:53:39

---
+++ htm/JurosParcelas.js
@@ -0,0 +1,15 @@
+//Informe abaixo os juros para parcelamento em 1x, 2x, 3x, etc.
+var Juros=new Array(0);
+/* Juros[0]=0; //1x (à vista)
+Juros[1]=4,590; //2x
+Juros[2]=4,590; //3x
+Juros[3]=4,590; //4x
+Juros[4]=4,590; //5x
+Juros[5]=4,590; //6x
+Juros[6]=4,590; //7x
+Juros[7]=4,790; //8x
+Juros[8]=4,790; //9x
+Juros[9]=4,790; //10x
+Juros[10]=4,790; //11x
+Juros[11]=4,790; //12x */
+
